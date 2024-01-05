e7193893faca68fb04b360c8557d36e9383ca020
Lilupa Karu <lilupa.karu@author-it.com>
2024-01-05 22:16:40 +1300

    Initial commit

---
+++ test/coffee-run-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CoffeeRunCdk from '../lib/coffee-run-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/coffee-run-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CoffeeRunCdk.CoffeeRunCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
